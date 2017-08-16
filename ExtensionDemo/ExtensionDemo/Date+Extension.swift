4eb9f70f26748326d75b79a6466017d9820a19de
xiejian <jx.mango@gmail.com>
2017-08-16 15:09:17 +0800

    add pod files

---
+++ ExtensionDemo/ExtensionDemo/Date+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  Date+Extension.swift
+//  ExtensionDemo
+//
+//  Created by Mstarc on 2017/8/11.
+//  Copyright © 2017年 com.mstarc.app. All rights reserved.
+//
+
+import Foundation
+
+
+extension Date{
+
+    func transformString(format:String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        formatter.dateFormat = format
+        formatter.timeZone = TimeZone(secondsFromGMT: 8)!
+        return formatter.string(from: self)
+    }
+
+
+
+
+}
